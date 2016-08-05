cce8207cf8f6a8face1a1ee8f502c836b638b466
Emerson Jr <emerson@mentesnotaveis.com.br>
2016-08-05 14:35:58 -0300

    Reestruturação do site

--- src/main.js
+++
@@ -1,7 +0,0 @@
-(function() {
-    angular.module("myApp", [ "ngRoute" ]);
-    
-    angular.element(document).ready(function() {
-      angular.bootstrap(document, ['myApp']);
-    });
-})();
--- src/scripts/controllers/index.controller.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    var IndexController = function($scope) {
-    }
-    
-    IndexController.$inject = [ "$scope" ];
-    
-    angular.module("myApp")
-        .controller("IndexController", IndexController);
-})();
