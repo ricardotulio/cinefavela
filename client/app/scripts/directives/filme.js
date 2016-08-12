e730da05141b34aee408ddc25e3c8851ea3d1f4b
Emerson Jr <emerson@mentesnotaveis.com.br>
2016-08-12 13:56:12 -0300

    Implementação do Modal de cadastro de usuario, login e visualização de Filme

---
+++ client/app/scripts/directives/filme.js
@@ -0,0 +1,14 @@
+(function() {
+	var FilmeDirective = function() {
+		return {
+			restrict: 'E'
+			scope: {
+				filme: '=filme'
+			},
+			templateUrl : 'views/directives/filme.html'
+		};
+	}
+
+	angular.module('app').directive('filme', FilmeDirective);
+	
+})();
